4d15cac90bc9270e1800f61ded6ba5f6080c5010
Ebru Karakaş <karakasebru2525@gmail.com>
2025-03-12 00:05:36 +0300

    Ocak_odev4

---
+++ ocak_odev4/ode.py
@@ -0,0 +1,6 @@
+def find_min_max():
+    numbers = [int(input("Bir sayı girin: ")) for _ in range(5)]
+    print("En küçük sayı:", min(numbers))
+    print("En büyük sayı:", max(numbers))
+
+find_min_max()
